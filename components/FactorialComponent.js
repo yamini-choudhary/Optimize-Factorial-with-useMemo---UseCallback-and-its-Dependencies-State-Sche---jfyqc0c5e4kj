04b0e0618f9ee557c9f5bb7a31008e35b46818d2
yamini-choudhary <120270758+yamini-choudhary@users.noreply.github.com>
2023-10-15 21:01:25 +0530

    Update FactorialComponent.js

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -1,12 +1,17 @@
-import { useMemo } from 'react';
-
+"use client"
+import { useMemo } from "react";
 function factorial(n) {
-  if (n <= 1) return 1;
+ if (n <= 1){
+  return 1;
+  }
+  else{
   return n * factorial(n - 1);
+  }
 }
-
-export default function FactorialComponent({ number }) {
-  const result = useMemo(() => factorial(number), [number]);
-
-  return <div className='result'>The factorial of {number} is {result}</div>;
+export default function FactorialComponent({val}) {
+  if(val===""){
+    return <div className='result'>The factorial of {val} is 1</div>;;
+  }
+  const fact=useMemo(()=>factorial(parseInt(val)),[val]);
+  return <div className='result'>The factorial of {val} is {fact}</div>;
 }
